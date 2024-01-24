d5b06b9f46e048776f058cdeb52319193cc1b74b
Denis Siliukov <abhem21rus@mail.ru>
2024-01-24 07:29:05 +0300

    Project initialization

---
+++ src/main/java/com/EmployeeBook/Homework15Stream/Homework15StreamApplication.java
@@ -0,0 +1,13 @@
+package com.EmployeeBook.Homework15Stream;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Homework15StreamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Homework15StreamApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/EmployeeBook/Homework15Stream/Homework15StreamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.EmployeeBook.Homework15Stream;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Homework15StreamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
